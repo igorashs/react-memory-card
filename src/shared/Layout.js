ba19a679ae11bffe81906831b3e7861c07e4a2e7
igorashs <igorashsirbu@gmail.com>
2020-12-21 22:39:50 +0200

    fix rules

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -9,8 +9,8 @@ export const Wrapper = styled.div`
 `;
 
 export const Modal = styled.div`
-  width: 100vw;
-  height: 100vh;
+  min-width: 100vw;
+  min-height: 100vh;
   position: absolute;
   display: flex;
   flex-direction: column;
