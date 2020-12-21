643e229484eeb97a0c7bbf6d18e9a03917dcc0f4
igorashs <igorashsirbu@gmail.com>
2020-12-21 22:31:48 +0200

    change to viewport units

--- src/shared/Layout.js
+++ src/shared/Layout.js
@@ -9,8 +9,8 @@ export const Wrapper = styled.div`
 `;
 
 export const Modal = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   position: absolute;
   display: flex;
   flex-direction: column;
